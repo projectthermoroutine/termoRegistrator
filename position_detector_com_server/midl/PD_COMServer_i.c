5320c54a58db72a1db0b5540717c5c40fdf0119f
thermoroutine@gmail.com <thermoroutine@gmail.com>
2015-04-03 19:20:14 +0400

    Revert "revert"
    This reverts commit ffe676bd8711a99941cf6d18aec96a0f367546b7.

--- position_detector_com_server/midl/PD_COMServer_i.c
+++ position_detector_com_server/midl/PD_COMServer_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Fri Apr 03 15:03:03 2015
+/* at Fri Mar 27 14:36:43 2015
  */
 /* Compiler settings for ..\PD_COMServer.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
