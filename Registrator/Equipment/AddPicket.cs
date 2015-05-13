8ec6f96f061fafc6cd1219997e6d565dfd6cfe09
projectthermoroutine <thermoroutine@gmail.com>
2015-05-13 14:16:23 +0300

    picket bux fix

--- Registrator/Equipment/AddPicket.cs
+++ Registrator/Equipment/AddPicket.cs
@@ -45,8 +45,9 @@ namespace Registrator.Equipment
 
         }
 
-        public void Pickets(int picketNumberArg, ref Pickets PicketsArg, EquPath equPath_Arg, EquLine equLine_Arg)
+        public void Pickets(int picketNumberArg, ref Pickets PicketsArg, EquPath equPath_Arg, EquLine equLine_Arg, Peregons peregonArg)
         {
+            peregonObj = peregonArg;
             peregonNumber = picketNumberArg;
             TxtBx_GroupName.Enabled = false;
             PicketsObj = PicketsArg;
