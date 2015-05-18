15e78eb34a24262283713bb2514242087c9794e8
projectthermoroutine <thermoroutine@gmail.com>
2015-05-18 14:36:00 +0300

    subquery bug fix

--- Registrator/Equipment/ProcessEquipment.cs
+++ Registrator/Equipment/ProcessEquipment.cs
@@ -198,25 +198,29 @@ namespace Registrator.Equipment
 
                 if (curline != -1)
                 {
-                    try
+                    if (DBHelper.subquery != null)
                     {
-                        setLine(curline, Convert.ToInt32(frameInfo.coordinate.path));
-                        path_detected = true;
-                        direction = frameInfo.coordinate.direction;
-                        //------------------------------------------------------- PROCESS EQUIPMENT ------------------------------------------------------------
-                        process(ref frameInfo);
-                        //--------------------------------------------------------------------------------------------------------------------------------------
-                    }
-                    catch (FormatException e)
-                    {
-                        path_detected = false;
-                        Log.Warn("could not be detect path number\n");
-                        return;
+                        try
+                        {
+                            setLine(curline, Convert.ToInt32(frameInfo.coordinate.path));
+                            path_detected = true;
+                            direction = frameInfo.coordinate.direction;
+                            //------------------------------------------------------- PROCESS EQUIPMENT ------------------------------------------------------------
+                            process(ref frameInfo);
+                            //--------------------------------------------------------------------------------------------------------------------------------------
+                        }
+                        catch (FormatException e)
+                        {
+                            path_detected = false;
+                            Log.Warn("could not be detect path number\n");
+                            return;
+                        }
                     }
                 }
             }
             else
-                process(ref frameInfo);
+                if (DBHelper.subquery != null)
+                    process(ref frameInfo);
 #endif
         }
 
