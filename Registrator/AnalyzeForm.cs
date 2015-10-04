c08f45df2ce119724bf9946f7a848726b1fb2edc
thermoroutine@gmail.com <thermoroutine@gmail.com>
2015-10-04 13:57:59 +0400

    рефакторинг + добавлен проект модульных тестов для проекта Registrator

--- Registrator/AnalyzeForm.cs
+++ Registrator/AnalyzeForm.cs
@@ -39,45 +39,17 @@ namespace Registrator
 
         }
 
-
-        class LIST_ITEM
+        List<Registrator.DB.ResultEquipCodeFrame> get_objects_by_coordinate(long coordinate, long max_offset_in_cm)
         {
-            public LIST_ITEM()
-            { objectId = 0; object_coordinate = 0; nearest_frame_index = 0; frame_coordinate = 0; frame_timestamp = 0.0; }
-            public LIST_ITEM(int objId, long obj_coordinate):this()
-            {
-                objectId = objId; object_coordinate = obj_coordinate;
-            }
-            public int objectId;
-            public long object_coordinate;
-            public Int32 nearest_frame_index;
-            public long frame_coordinate;
-            public double frame_timestamp;
-        }
-
-        List<LIST_ITEM> _processing_objects;
-        List<LIST_ITEM> _processed_objects;
-
-
-        List<LIST_ITEM> get_objects_by_coordinate(long coordinate, long max_offset_in_cm)
-        {
-            List<LIST_ITEM> result = new List<LIST_ITEM>();
-
-            var objects = _db_controller.get_objects_by_coordinate(coordinate / 10, max_offset_in_cm);
-            foreach (var db_object in objects)
-            {
-                result.Add(new LIST_ITEM (db_object.Code, db_object.shiftLine));
-            }
-
-            return result;
+            return (List<Registrator.DB.ResultEquipCodeFrame>)_db_controller.get_objects_by_coordinate(coordinate / 10, max_offset_in_cm);
         }
 
         private void Analyze(BackgroundWorker worker)
         {
             var number_frames = m_movieTransit.FramesCount();
 
-            _processing_objects = new List<LIST_ITEM>();
-            _processed_objects = new List<LIST_ITEM>();
+            ChoiceFrameObject choice_frames = new ChoiceFrameObject();
+            choice_frames.SaveObjectFrameProcessHandler += save_object_termogramme;
 
             for (int i = 0; i < number_frames; i++)
             {
@@ -109,161 +81,30 @@ namespace Registrator
                 //TODO: устанавливаем линию и путь в контроллер БД
 
                 var objects = get_objects_by_coordinate(coordinate.coordinate, max_frame_distance_cm);
-                if (objects.Count == 0)
+                choice_frames.process_objects(objects,
+                delegate(Registrator.DB.ResultEquipCodeFrame obj, out int objId, out long obj_coord)
                 {
-                    _processed_objects.Clear();
-                    save_objects_termogrammes(_processing_objects);
-                    _processed_objects = _processing_objects;
-                    _processing_objects.Clear();
-                }
-                else 
-                {
-                    process_objects(objects, coordinate.coordinate, i, frame_data_time);
-                }
+                    objId = obj.Code;
+                    obj_coord = obj.shiftLine;
+                },
+                coordinate.coordinate, i, frame_data_time);
 
                 worker.ReportProgress(100 * (i + 1) / number_frames);
             }
+
+            choice_frames.close();
         }
 
-        void process_objects(List<LIST_ITEM> objects, long frame_coordinate, int frame_index, double frame_timestamp)
+        void save_object_termogramme(object sender, SaveObjectFrameProcessEvent arg)
         {
-            List<LIST_ITEM> not_processed_objects = new List<LIST_ITEM>();
-
-            foreach (var db_object in objects) 
-            {
-                bool processed = false;
-                foreach(var processed_object in _processed_objects)
-                {
-                    if (processed_object.objectId == db_object.objectId)
-                    {
-                        processed = true;
-                        break;
-                    }
-                }
-                if (!processed)
-                {
-                    db_object.frame_coordinate = frame_coordinate;
-                    db_object.nearest_frame_index = frame_index;
-                    db_object.frame_timestamp = frame_timestamp;
-                    not_processed_objects.Add(db_object);
-                }
-
-            }
-
-            List<LIST_ITEM> objects_for_save = new List<LIST_ITEM>();
-            List<LIST_ITEM> new_objects = new List<LIST_ITEM>();
-
-            foreach (var db_object in not_processed_objects)
-            {
-                LIST_ITEM nearest_frame = null;
-                bool add_frame = true;
-                foreach (var processing_object in _processing_objects)
-                {
-                    if (processing_object.objectId == db_object.objectId)
-                    {
-                        add_frame = false;
-                        var prev_distance = calc_frame_object_distance(processing_object.frame_coordinate, processing_object.object_coordinate);
-                        var current_distance = calc_frame_object_distance(db_object.frame_coordinate, db_object.object_coordinate);
-
-                        if (prev_distance < current_distance)
-                        {
-                            nearest_frame = processing_object;
-                        }
-                        else
-                        {
-                            processing_object.frame_coordinate = db_object.frame_coordinate;
-                            processing_object.nearest_frame_index = db_object.nearest_frame_index;
-                            processing_object.frame_timestamp = db_object.frame_timestamp;
-                        }
-
-                        break;
-                    }
-                }
-                if (nearest_frame != null)
-                    objects_for_save.Add(nearest_frame);
-
-                if (add_frame)
-                {
-                    new_objects.Add(db_object);
-                }
-            }
-
-            delete_items_from_list(_processed_objects, delegate(LIST_ITEM processed_object)
-            {
-                var current_distance = calc_frame_object_distance(frame_coordinate, processed_object.object_coordinate);
-                return max_frame_distance_cm * 10 < current_distance ? true : false;
-            });
-
-            _processed_objects.AddRange(new_objects);
-
-            save_objects_termogrammes(objects_for_save);
-
-            delete_items_from_list(_processed_objects, delegate(LIST_ITEM processing_object)
-            {
-                int index_for_delete = -1;
-                for (int i = 0; i < objects_for_save.Count; i++)
-                {
-                    var saved_object = objects_for_save[i];
-                    if(processing_object.objectId == saved_object.objectId)
-                    {
-                        index_for_delete = i;
-                        break;
-                    }
-                }
-
-                if(index_for_delete >= 0){
-                    objects_for_save.RemoveAt(index_for_delete);
-                    return true;
-                }
-
-                return false;
-            });
-
+            var termogramm_name = generate_termogramm_name(arg.ObjectId, arg.FrameIndex, arg.FrameCoord, arg.FrameTimeStamp);
+            //TODO: сохраняем данные в БД и/или на диск
         }
 
-        void delete_items_from_list<T>(List<T> list_values,Predicate<T> match)
-        {
-            if (list_values.Count > 0)
-            {
-                List<int> indxs_for_delete = new List<int>();
-                for (int i = 0; i < list_values.Count; i++)
-                {
-                    var value = list_values[i];
-                    if(match(list_values[i]))
-                    {
-                        indxs_for_delete.Add(i);
-                    }
-                }
-
-                if (indxs_for_delete.Count > 0)
-                {
-                    int count_deletes = 0;
-                    foreach (var index_for_delete in indxs_for_delete)
-                    {
-                        list_values.RemoveAt(index_for_delete - count_deletes);
-                        count_deletes++;
-                    }
-
-                }
-            }
-
-        }
-
-        long calc_frame_object_distance(long frame_coord,long object_coord)
-        {
-            return frame_coord <= object_coord ? object_coord - frame_coord : frame_coord - object_coord;
-        }
-
-        void save_objects_termogrammes(List<LIST_ITEM> objects)
-        {
-            foreach(var db_object in objects)
-            {
-                var termogramm_name = generate_termogramm_name(db_object);
-                //TODO: сохраняем данные в БД и/или на диск
-            }
-        }
-
-        string generate_termogramm_name(LIST_ITEM info)
+        string generate_termogramm_name(int objectId,
+                    int frame_index,
+                    long frame_coord,
+                    double frame_timestamp)
         {
             //TODO: генерация либо имя термограммы для БД, либо имя файла с термограммой
             return "";  
