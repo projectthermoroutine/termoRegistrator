fcb218a2b44a6653c5847a9e33b59e9171c88892
boksterpink@gmail.ru <boksterpink@gmail.ru>
2016-09-11 00:38:36 +0400

    path view bug fix

--- Registrator/PlayerPanel/PlayerPanel_Threads.cs
+++ Registrator/PlayerPanel/PlayerPanel_Threads.cs
@@ -81,7 +81,7 @@ namespace Registrator
                         if (frame_info.image_info.width == 1024) SetPlayerControlImage((byte[])raster, 1024, 768);
                         else SetPlayerControlImage((byte[])raster, 640, 480);
 
-                        cur_coord = (long)frame_info.coordinate.coordinate + frame_info.coordinate.camera_offset;
+                        cur_coord = (long)frame_info.coordinate.coordinate;// +frame_info.coordinate.camera_offset;
 
                         var measure = new CTemperatureMeasure(frame_info.measure.tmin, frame_info.measure.tmax, frame_info.measure.tavr,
                             frame_info.measure.object_tmin, frame_info.measure.object_tmax, 0,                            
@@ -103,10 +103,10 @@ namespace Registrator
                             get_areas_temperature_measure();
                         }
                         //------------------------------------------------------- PROCESS EQUIPMENT ------------------------------------------------------------
-                        if (!apply_camera_offset)
-                        {
-                            current_camera_offset = frame_info.coordinate.camera_offset;
-                        }
+                        //if (!apply_camera_offset)
+                        //{
+                        //    current_camera_offset = frame_info.coordinate.camera_offset;
+                        //}
                         if (equipmentMonitor != null)
                         {
                             Invoke(new EventHandler(delegate { equipmentMonitor.track_process(ref frame_info); }));
