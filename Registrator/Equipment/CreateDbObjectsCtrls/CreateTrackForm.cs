37187af5999d8fd21c57c03128ff96cccd87e57b
projectthermoroutine <thermoroutine@gmail.com>
2018-04-22 01:57:11 +0300

    added query what corrects equipment offset relative start coordinate of line

--- Registrator/Equipment/CreateDbObjectsCtrls/CreateTrackForm.cs
+++ Registrator/Equipment/CreateDbObjectsCtrls/CreateTrackForm.cs
@@ -10,37 +10,7 @@ using System.Windows.Forms;
 
 namespace Registrator.Equipment.CreateDbObjectsCtrls
 {
-    public class IDbObjectCreation
-    {
-        public event EventHandler<DbObjectEventArg> DbObjectAddedEvent;
-        public void DbObjectAdded(EquDbObject db_object)
-        {
-            EventHandler<DbObjectEventArg> handler = DbObjectAddedEvent;
-            if (handler != null)
-                handler(this, new DbObjectEventArg(db_object));
-        }
-    }
-
-    public class DbObjectEventArg : EventArgs
-    {
-        public DbObjectEventArg(EquDbObject db_object){
-            DbObject = db_object;
-        }
-        public DbObjectEventArg(EquDbObject db_object, EquDbObject[] db_objects)
-        {
-            DbObject = db_object;
-            DbObjects = db_objects;
-        }
-        public DbObjectEventArg(EquDbObject[] db_objects, bool left_picket)
-        {
-            DbObjects = db_objects;
-            leftPicket = left_picket;
-        }
-        public EquDbObject DbObject {get;private set; }
-        public EquDbObject[] DbObjects { get; private set; }
-        public bool leftPicket { get; private set; }
-    }
-   
+    
     public partial class CreateTrackForm : Form
     {
         public event EventHandler<DbObjectEventArg> TrackAddedEvent;
